0591ceab9c0d575ef21227776f1858acf7188f79
Gabriel-Batista <gabedbatista@gmail.com>
2018-09-26 10:06:46 -0400

    Done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,32 +1,47 @@
 import React, { Component } from 'react'
 import TransactionsList from './TransactionsList'
 import Search from './Search'
-import {transactions} from '../transactionsData'
+import { transactions } from '../transactionsData'
 
 class AccountContainer extends Component {
 
-  constructor() {
-    super()
+    constructor(props) {
+        super(props)
+        this.state = {
+            api: "https://boiling-brook-94902.herokuapp.com/transactions",
+            filter: "",
+            transactions: []
+        }
+    }
 
-    // get a default state working with the data imported from TransactionsData
-    // use this to get the functionality working
-    // then replace the default transactions with a call to the API
+    componentDidMount() {
+        fetch(this.state.api)
+            .then(res => res.json())
+            .then(json => this.setState({ transactions: json }))
+    }
 
-  }
+    handleChange= (event) => {
+        this.setState({filter: event.target.value})
+    }
 
-  handleChange(event) {
-    // your code here
-  }
+    filteredList= () => {
+        return this.state.transactions.filter((trans) => {
+            return (trans.category.includes(this.state.filter) || trans.description.includes(this.state.filter))
+        })
+    }
 
-  render() {
+    render() {
 
-    return (
-      <div>
-        <Search />
-        <TransactionsList />
-      </div>
-    )
-  }
+        return (
+            <div>
+                <Search 
+                    handleChange={this.handleChange}
+                    filter={this.state.filter}
+                />
+                <TransactionsList filteredList={this.filteredList}/>
+            </div>
+        )
+    }
 }
 
 export default AccountContainer
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
-const Search = () => {
-  return (
-    <div className="ui huge fluid icon input">
-      <input
-        type="text"
-        placeholder={"Search your Recent Transactions"}
-      />
-      <i className="circular search link icon"></i>
-    </div>
-  )
+const Search = (props) => {
+    return (
+        <div className="ui huge fluid icon input">
+            <input
+                type="text"
+                value={props.filter}
+                placeholder={"Search your Recent Transactions"}
+                onChange={props.handleChange}
+            />
+            <i className="circular search link icon"></i>
+        </div>
+    )
 }
 
 export default Search
