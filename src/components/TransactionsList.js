57f845811d3c1f365abecb61968a291a5462b4eb
Gina Lee <rjeanpierre90@gmail.com>
2018-09-26 10:47:09 -0400

    still working on filter

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,7 @@
 import React from 'react'
+import Transaction from './Transaction'
 
-const TransactionsList = () => {
-
+const TransactionsList = ({transactions}) => {
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -28,7 +28,7 @@ const TransactionsList = () => {
           </th>
         </tr>
 
-        {"... your code here..."}
+        {transactions.map(transaction=> <Transaction transaction={transaction}/>)}
 
       </tbody>
     </table>
