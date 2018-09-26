40340627a4d59856e296dd6710eeedc642482c30
robertobuso <robertobuso@gmail.com>
2018-09-26 10:08:33 -0400

    fetch done showing all transactions done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react'
 import TransactionsList from './TransactionsList'
 import Search from './Search'
-import {transactions} from '../transactionsData'
+// import {transactions} from '../transactionsData'
 
 class AccountContainer extends Component {
 
   constructor() {
     super()
-
+    this.state = {
+      allTransactions: []
+    }
     // get a default state working with the data imported from TransactionsData
     // use this to get the functionality working
     // then replace the default transactions with a call to the API
 
+  }
+
+  componentDidMount() {
+    fetch('https://boiling-brook-94902.herokuapp.com/transactions')
+    .then(r => r.json())
+    .then(r => this.setState({ allTransactions: r}))
   }
 
   handleChange(event) {
@@ -19,11 +27,10 @@ class AccountContainer extends Component {
   }
 
   render() {
-
     return (
       <div>
         <Search />
-        <TransactionsList />
+        <TransactionsList allTransactions={this.state.allTransactions}/>
       </div>
     )
   }
